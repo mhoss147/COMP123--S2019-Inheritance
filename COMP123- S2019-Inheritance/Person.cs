dd4ec862988caad6826e79a2d061708c16c33b4e
Mohammad Sorowar Hossain <mhoss147@my.centennialcollege.ca>
2019-06-29 15:01:18 -0400

    Added Person class

---
+++ COMP123- S2019-Inheritance/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123__S2019_Inheritance
+{/// <summary>
+/// this is the person class
+/// </summary>
+    class Person
+    {
+        //private instance variables
+        private string _name;
+        private int _age;
+        // public properties
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+        }
+        //constructors
+        /// <summary>
+        /// this is the constructor for the Person class
+        /// it takes 2 arguments - name (string) and age (int)
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        //public methods
+        /// <summary>
+        /// this method enables Person object to talk
+        /// </summary>
+        public void Talks()
+        {
+            Console.WriteLine(this.Name + " is talking");
+        }
+
+    }
+}
--- COMP123- S2019-Inheritance/Program.cs
+++ COMP123- S2019-Inheritance/Program.cs
@@ -10,6 +10,11 @@ namespace COMP123__S2019_Inheritance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n");
+            Person person = new Person("Mohammad", 20 );
+            person.Talks();
+            Console.WriteLine("\n");
+
         }
     }
 }
